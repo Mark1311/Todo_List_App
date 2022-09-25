a42a2740a1cf7d6b981fc03876abbbb294ba6115
BITTU KUMAR <82657410+Mark1311@users.noreply.github.com>
2022-09-25 17:52:48 +0530

    Add files via upload

---
+++ todo_List/src/App.js
@@ -0,0 +1,64 @@
+import "./index.css";
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteIems = (id) => {
+    console.log("deleted");
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <>
+      <div className="main_div">
+      
+        <div className="center_div">
+          <br />
+          <h1>Multi Task List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add A Items"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {/* <li>{inputList}</li> */}
+
+            {Items.map((itemval, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemval}
+                  onSelect={deleteIems}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
